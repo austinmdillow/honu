81a4895277540ebf88e4540f8b3e2f64a9cdb304
austinmdillow <austinmdillow@gmail.com>
2021-02-08 14:46:01 -0600

    adding second build

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 ##### build #####
 
 find . -iname "*.lua" | xargs luac -p || { echo 'luac parse test failed' ; exit 1; }
-find . -iname "tests/*.lua" | xargs luac -p || { echo 'luac parse test failed' ; exit 1; }
+busted tests/busted-test.lua 
 echo 'build passed'
